14f3edbe8093f11ebd0c6df8978bf6fe24299f9c
Serhii Zadorozhnii <wiseacreserj@gmail.com>
2025-03-07 05:11:43 +0200

    initial setup commit

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+const app = express();
+
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+console.log(process.env.MONGO_URI);
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("MongoDB conected!"))
+    .catch((err) => console.log("Connection error:", err));
+
+app.get("/", (req, res) => {
+    res.send("Server working!");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
