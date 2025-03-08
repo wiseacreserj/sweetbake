b5a1302d968d5d05ca4a3f39bf76c109071eeacb
Serhii Zadorozhnii <wiseacreserj@gmail.com>
2025-03-08 02:59:22 +0200

    add role to User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -6,6 +6,7 @@ const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, requried: true },
     phone: { type: String, requried: true },
+    role: { type: String, enum: ["user", "admin"], default: "user" },
     createdAt: { type: Date, default: Date.now },
 });
 
