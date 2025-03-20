2d9fafef4fef2812eb6e7a483b70fbbb8125db15
Serhii Zadorozhnii <wiseacreserj@gmail.com>
2025-03-20 16:36:39 +0200

    create userValidation.js

---
+++ backend/validation/userValidation.js
@@ -0,0 +1,52 @@
+import { body } from "express-validator";
+
+export const newUserValidation = [
+    body("username")
+        .exists()
+        .withMessage("Username is required")
+        .bail()
+        .isString()
+        .isLength({ min: 3, max: 300 })
+        .withMessage(
+            "Username must be a string min. length 3, max. length 300 symbols"
+        ),
+    body("email")
+        .exists()
+        .withMessage("Email is required")
+        .bail()
+        .isEmail()
+        .withMessage("Email must be valid email address"),
+    body("password")
+        .exists()
+        .withMessage("Password is required")
+        .bail()
+        .isString()
+        .isLength({ min: 8, max: 100 })
+        .withMessage(
+            "Password must be a string min. length 8, max. length 100 symbols"
+        ),
+    body("phone")
+        .exists()
+        .withMessage("Phone number is required")
+        .bail()
+        .isMobilePhone("any", { strictMode: true })
+        .withMessage("Phone number must be a valid international number"),
+];
+
+export const loginUserValidation = [
+    body("email")
+        .exists()
+        .withMessage("Email is required")
+        .bail()
+        .isEmail()
+        .withMessage("Email must be valid email address"),
+    body("password")
+        .exists()
+        .withMessage("Password is required")
+        .bail()
+        .isString()
+        .isLength({ min: 8, max: 100 })
+        .withMessage(
+            "Password must be a string min. length 8, max. length 100 symbols"
+        ),
+];
