b5a5ccc8d5ec91c5be685e23dd50527a0fe43b54
Serhii Zadorozhnii <wiseacreserj@gmail.com>
2025-03-28 20:21:26 +0200

    import global css files into main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import { store, persistor } from "./store/index.js";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+
+import "./index.css";
+import "./styles/reset.css";
+import "./styles/variables.css";
+import "./styles/global.css";
 
 import App from "./App.jsx";
 
