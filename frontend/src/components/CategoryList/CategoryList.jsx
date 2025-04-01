ee211c9ad004626745be1eff9c64cddbe87844a1
Serhii Zadorozhnii <wiseacreserj@gmail.com>
2025-04-01 05:01:42 +0300

    create CategoryList component

---
+++ frontend/src/components/CategoryList/CategoryList.jsx
@@ -0,0 +1,37 @@
+const CategoryList = () => {
+    const data = [
+        {
+            categoryName: "Breads",
+            imageUrl:
+                "https://static01.nyt.com/images/2024/10/08/multimedia/13EATrex-LD-briocherex-blfk/13EATrex-LD-briocherex-blfk-master768.jpg?quality=75&auto=webp",
+        },
+        {
+            categoryName: "Snacks",
+            imageUrl:
+                "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2023/9/22/QK905-kardea-brown-pimento-cheese-straws_s4x3.jpg.rend.hgtvcom.1280.960.suffix/1695412613868.webp",
+        },
+        {
+            categoryName: "Savory Pies",
+            imageUrl:
+                "https://assets.bonappetit.com/photos/58a5ffa6051644118326fbf9/1:1/w_2580,c_limit/spanakopita-pie.jpg",
+        },
+        {
+            categoryName: "Pastries",
+            imageUrl:
+                "https://images.stockcake.com/public/b/8/b/b8bca4f2-33d0-40cb-a267-d57d747713cc_large/assorted-breakfast-pastries-stockcake.jpg",
+        },
+        {
+            categoryName: "Desserts",
+            imageUrl:
+                "https://www.southernliving.com/thmb/7oCQ4L6HaNIryOHL2Ttxvm8rFHw=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Heaven-in-a-Bowl_3x2-066-6b099d564c274d04b2984f8c1d37ec8f.jpg",
+        },
+        {
+            categoryName: "Drinks",
+            imageUrl:
+                "https://www.southernliving.com/thmb/zRo2u_HuXVt3tMpsARNOQTtQRH4=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Southern_Living_Shirley_Temple_SEO49-beauty-1-08cefeefb1a34bea898bd871591a5ec5.jpg",
+        },
+    ];
+    return <></>;
+};
+
+export default CategoryList;
