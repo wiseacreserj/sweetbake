d1093f394d708bc2123b0ef6c20997585e9e2734
Serhii Zadorozhnii <wiseacreserj@gmail.com>
2025-04-14 23:43:12 +0300

    add basic styles to LoginPage

--- frontend/src/pages/LoginPage/LoginPage.jsx
+++ frontend/src/pages/LoginPage/LoginPage.jsx
@@ -1,8 +1,10 @@
+import styles from "./LoginPage.module.css";
+
 const LoginPage = () => {
     return (
-        <>
+        <div className={styles.loginPageContainer}>
             <p>Login Page</p>
-        </>
+        </div>
     );
 };
 
