936e85ad3071783d107df74e6110202ea8e28b5b
Serhii Zadorozhnii <wiseacreserj@gmail.com>
2025-04-20 22:56:44 +0300

    connect LoginForm with react-hook-form

--- frontend/src/components/LoginForm/LoginForm.jsx
+++ frontend/src/components/LoginForm/LoginForm.jsx
@@ -1,7 +1,34 @@
+import { useForm } from "react-hook-form";
+
 import styles from "./LoginForm.module.css";
 
 const LoginForm = () => {
-    return <div>Login Form</div>;
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm();
+
+    return (
+        <div className={styles.formContainer}>
+            <form onSubmit={handleSubmit(() => console.log(watch()))}>
+                <label htmlFor="email">Email</label>
+                <input
+                    type="email"
+                    {...register("email")}
+                    placeholder="Email"
+                />
+                <label htmlFor="password">Password</label>
+                <input
+                    type="email"
+                    {...register("password")}
+                    placeholder="Password"
+                />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
 };
 
 export default LoginForm;
